db47e5f3e2d2a8303d0d781538d72e909b3b905e
m4chado <enzo653fm@gmail.com>
2023-01-10 16:11:52 -0300

    add typescript

---
+++ src/pages/LandingPage/index.jsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+	return (
+		<button className="joinBtn" type="button" onClick={() => setIsStart(!isStart)}>
+			Iniciar
+		</button>
+	);
+};
+
+export default LandingPage;
